35a3da28bcae05467a779a140e85e55cc024db1e
Bruce Schultz <bschultz013@gmail.com>
2023-10-07 09:54:13 +0200

    update node_helper to use fetch

--- node_helper.js
+++ node_helper.js
@@ -6,26 +6,18 @@
  */
 
 var NodeHelper = require('node_helper');
-var request = require('request');
 
 module.exports = NodeHelper.create({
 	start: function () {
 		console.log('MMM-Dad-Jokes helper started');
 	},
 
-	getJoke: function (url) {
+	getJoke: async function () {
 		var parent = this; // save this object
-		request({ url: 'https://icanhazdadjoke.com',
-			headers:{
-				'Accept':'application/json',
-				'User-Agent': 'MMM-Dad-Jokes (https://github.com/echang15/MMM-Dad-Jokes)'
-			},
-			method: 'GET' }, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var result = JSON.parse(response.body);
-				parent.sendSocketNotification('JOKE_RESULT', result);
-			}
-		});
+
+		fetch('https://icanhazdadjoke.com').then(response => {
+			parent.sendSocketNotification('JOKE_RESULT', response.json())
+		}).catch(err => console.error(err));
 	},
 
 	socketNotificationReceived: function(notification, payload) {
